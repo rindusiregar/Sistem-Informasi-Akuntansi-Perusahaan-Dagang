a821fb58d6bc67ce3ae089d711058e55d795c8f8
rindusiregar <152129889+rindusiregar@users.noreply.github.com>
2023-12-11 05:21:57 +0700

    Add files via upload

---
+++ database/migrations/2023_12_10_065959_create_barang_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang', 50);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->integer('stok_tersedia');
+            $table->integer('id_supplier')->unsigned();;
+            $table->timestamps();
+
+            $table->foreign('id_supplier')->references('id')->on('supplier');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
