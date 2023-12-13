9dab31dbae96266e3e5d91f117161c86912fc120
rindusiregar <152129889+rindusiregar@users.noreply.github.com>
2023-12-13 20:22:30 +0700

    Add files via upload

---
+++ views/layouts/master.blade.php
@@ -0,0 +1,141 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Sistem Penjualan</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendor/font-awesome/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendor/linearicons/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('admin/assets/vendor/chartist/css/chartist-custom.css') }}">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/main.css') }}">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="{{ asset('admin/assets/css/demo.css') }}">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('admin/assets/img/apple-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('admin/assets/img/favicon.png') }}">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper" style="background-color: #ff9999">
+		<!-- NAVBAR -->
+        @include('layouts.include._navbar')
+		<!-- END NAVBAR -->
+		<!-- LEFT SIDEBAR -->
+		@include('layouts.include._sidebar')
+		<!-- END LEFT SIDEBAR -->
+        @yield('content')
+		<!-- MAIN -->
+		<!-- END MAIN -->
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<script src="{{ asset('admin/assets/vendor/jquery/jquery.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/vendor/chartist/js/chartist.min.js') }}"></script>
+	<script src="{{ asset('admin/assets/scripts/klorofil-common.js') }}"></script>
+	<script>
+	$(function() {
+		var options;
+
+		var data = {
+			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			series: [
+				[200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
+			]
+		};
+
+		// line chart
+		options = {
+			height: "300px",
+			showPoint: true,
+			axisX: {
+				showGrid: false
+			},
+			lineSmooth: false,
+		};
+
+		new Chartist.Line('#demo-line-chart', data, options);
+
+		// bar chart
+		options = {
+			height: "300px",
+			axisX: {
+				showGrid: false
+			},
+		};
+
+		new Chartist.Bar('#demo-bar-chart', data, options);
+
+
+		// area chart
+		options = {
+			height: "270px",
+			showArea: true,
+			showLine: false,
+			showPoint: false,
+			axisX: {
+				showGrid: false
+			},
+			lineSmooth: false,
+		};
+
+		new Chartist.Line('#demo-area-chart', data, options);
+
+
+		// multiple chart
+		var data = {
+			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			series: [{
+				name: 'series-real',
+				data: [200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
+			}, {
+				name: 'series-projection',
+				data: [240, 350, 360, 380, 400, 450, 480, 523, 555, 600, 700, 800],
+			}]
+		};
+
+		var options = {
+			fullWidth: true,
+			lineSmooth: false,
+			height: "270px",
+			low: 0,
+			high: 'auto',
+			series: {
+				'series-projection': {
+					showArea: true,
+					showPoint: false,
+					showLine: false
+				},
+			},
+			axisX: {
+				showGrid: false,
+
+			},
+			axisY: {
+				showGrid: false,
+				onlyInteger: true,
+				offset: 0,
+			},
+			chartPadding: {
+				left: 20,
+				right: 20
+			}
+		};
+
+		new Chartist.Line('#multiple-chart', data, options);
+
+	});
+	</script>
+
+</body>
+
+</html>
